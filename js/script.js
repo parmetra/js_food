86fe1e99cbed72ad94a8491b044f9b35b5258a7b
Artyom K <par_metra@mail.ru>
2023-06-05 12:03:03 +0300

    Урок 94. Навигация для слайдов

--- js/script.js
+++ js/script.js
@@ -426,12 +426,14 @@ window.addEventListener("DOMContentLoaded", () => {
 		}
 		slidesField.style.transform = `translateX(-${offset}px)`;
 		currentSlide.textContent = isLessThanTen(counterSlides);// выводим значение текущего слайда
+		
+		dotsNavigating(dots, counterSlides);
 	});
 
 	prevSlideBtn.addEventListener("click", () => {	// кнопка слайдов назад
 		counterSlides--;
 		if(offset == 0 && (counterSlides < 1)) {	// проверка на то, что слайд и счётчик являются последними
-			offset = +widthSlide.slice(0, widthSlide.length - 2) * (imageSlide.length - 1)
+			offset = +widthSlide.slice(0, widthSlide.length - 2) * (imageSlide.length - 1);
 			counterSlides = imageSlide.length;
 		}
 		else {
@@ -439,6 +441,8 @@ window.addEventListener("DOMContentLoaded", () => {
 		}
 		slidesField.style.transform = `translateX(-${offset}px)`;
 		currentSlide.textContent = isLessThanTen(counterSlides);// выводим значение текущего слайда
+		
+		dotsNavigating(dots, counterSlides);
 	});
 
 	function isLessThanTen(number) { //функция добавляет `0` перед числом, если оно меньше 10
@@ -446,6 +450,75 @@ window.addEventListener("DOMContentLoaded", () => {
 			number = `0${number}`;
 		}
 		return number;
+	}
+
+	// Навигация в слайдере
+	let slider = document.querySelector(".offer__slider");
+
+	slider.style.position = "relative";
+
+	const indicators = document.createElement("ol"),
+		  dots = [];
+	indicators.classList.add("carousel-indicators");
+	indicators.style.cssText = `
+		position: absolute;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		z-index: 15;
+		display: flex;
+		justify-content: center;
+		margin-right: 15%;
+		margin-left: 15%;
+		list-style: none;
+	`;
+	slider.append(indicators);
+
+	for (let i = 0; i < imageSlide.length; i++) {
+		const dot = document.createElement("li");
+		dot.setAttribute("data-slide-to", i + 1);
+		dot.style.cssText = `
+			box-sizing: content-box;
+			flex: 0 1 auto;
+			width: 30px;
+			height: 6px;
+			margin-right: 3px;
+			margin-left: 3px;
+			cursor: pointer;
+			background-color: #fff;
+			background-clip: padding-box;
+			border-top: 10px solid transparent;
+			border-bottom: 10px solid transparent;
+			opacity: .5;
+			transition: opacity .6s ease;
+		`;
+		if (i == 0) {
+			dot.style.opacity = 1;
+		}
+		indicators.append(dot);
+		dots.push(dot);
+	}
+
+	dots.forEach (dot => {
+		dot.addEventListener("click", (e) => {
+			const slideTo = e.target.getAttribute('data-slide-to');
+
+			counterSlides = slideTo;
+			offset = +widthSlide.slice(0, widthSlide.length - 2) * (slideTo - 1);
+			slidesField.style.transform = `translateX(-${offset}px)`;
+
+			currentSlide.textContent = isLessThanTen(slideTo);// выводим значение текущего слайда
+
+			dotsNavigating(dots, counterSlides);
+
+		});
+	});
+	
+	function dotsNavigating(dots, counterSlides) {
+		dots.forEach(dot => {
+			dot.style.opacity = ".5";
+		});
+		dots[counterSlides - 1].style.opacity = "1";
 	}
 
 });
